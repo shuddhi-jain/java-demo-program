6f160ed156306c6fd1c6f6fcebbb38567fbc6b3c
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2024-03-21 20:05:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main
   {
     public static void main(String args[]){
-      System.out.prinln("Starting up with jenkins");
+      System.out.println("Starting up with jenkins");
     }
   }
