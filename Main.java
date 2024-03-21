62c9874bfe0262545c80d5814718c03c54f0ede5
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2024-03-21 20:07:16 +0530

    Stating the changes

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main
   {
     public static void main(String args[]){
       System.out.println("Starting up with jenkins");
+      System.out.println("Everytime it checks and updates");
     }
   }
